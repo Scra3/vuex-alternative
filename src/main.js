5fff7cd27d556a711deaeac8d6c2e7360496e90b
alban bertolini <alban@sogilis.com>
2020-10-14 23:48:51 +0200

    remove vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
 import vuetify from "./plugins/vuetify";
 import vueUseRepositories from "./plugins/repositories";
 import router from "./plugins/router";
@@ -10,7 +9,6 @@ Vue.config.productionTip = false;
 vueUseRepositories();
 
 new Vue({
-  store,
   vuetify,
   router,
   render: h => h(App)
