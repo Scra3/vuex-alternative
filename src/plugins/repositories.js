fa3d9b75e0799f4e35817d559784683f9743b1b0
alban bertolini <alban@sogilis.com>
2020-10-17 00:33:25 +0200

    keep plugin in repositories plugin file

--- src/plugins/repositories.js
+++ src/plugins/repositories.js
@@ -1,14 +1,7 @@
-import Vue from "vue";
 import shoppingCart from "@/repositories/shoppingCart";
 
-// TODO: move this in main (to prevent any constraint on how it is used) ?
-export const shoppingCartPlugin = {
+export const ShoppingCart = {
   install: function(Vue) {
     Vue.prototype.$shoppingCart = shoppingCart;
   }
-};
-
-export default () => {
-  // TODO: move this in main (to prevent any constraint on how it is used) ?
-  Vue.use(shoppingCartPlugin);
 };
