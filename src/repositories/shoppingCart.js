34ae091d923a0470cdbc565884ea123256bf103f
alban bertolini <alban@sogilis.com>
2020-10-28 22:57:51 +0100

    replace splice by filter in shoppingCart repo

--- src/repositories/shoppingCart.js
+++ src/repositories/shoppingCart.js
@@ -1,6 +1,12 @@
 import EventBus from "@/event-bus";
 import { NEW_PRODUCT_IN_SHOPPING_CART_EVENT } from "@/constants";
 
+const generateRandomId = () => {
+  return Math.random()
+    .toString(36)
+    .substr(2, 9);
+};
+
 export default {
   state: {
     products: []
@@ -8,13 +14,16 @@ export default {
   add(product) {
     EventBus.$emit(NEW_PRODUCT_IN_SHOPPING_CART_EVENT);
 
-    this.state.products.push(product);
+    this.state.count++;
+    this.state.products.push({
+      ...product,
+      shoppingCartId: generateRandomId()
+    });
   },
   remove(productToRemove) {
-    const index = this.state.products.findIndex(
-      product => product.id === productToRemove.id
+    this.state.products = this.state.products.filter(
+      product => product.shoppingCartId !== productToRemove.shoppingCartId
     );
-    this.state.products.splice(index, 1);
   },
   getTotalPrice() {
     return this.state.products.reduce((total, product) => {
